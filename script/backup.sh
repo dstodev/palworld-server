cb3aac40212a8af148e0689e63919aa3fbde2cab
Daniel Stotts <dstodev@gmail.com>
2024-01-20 20:19:35 -0800

    Helper doc

--- script/backup.sh
+++ script/backup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+# Add to crontab, running every hour:
+#  crontab -e
+#  0 * * * * /absolute/path/to/script/backup.sh >/dev/null 2>&1
+
 script_dir="$(cd "$(dirname "$0")" && env pwd --physical)"
 source_dir="$script_dir/.."
 compose_yml=$(readlink --canonicalize "$source_dir/docker/compose.yml")
