ee95cb7c0cbb40403e238b6857c84d0b849e6d57
Daniel Stotts <dstodev@gmail.com>
2024-01-25 19:25:32 -0800

    Backup before shutdown

--- script/stop-server.sh
+++ script/stop-server.sh
@@ -6,9 +6,10 @@ script_dir="$(cd "$(dirname "$0")" && env pwd --physical)"
 running_container=$(docker container list --filter name=palworld-server --quiet)
 
 if [ -n "$running_container" ]; then
+	"$script_dir/backup.sh"
+
 	# palworld commands
 	rcon="$script_dir/send-rcon.sh"
-	"$rcon" save | grep --ignore-case --invert-match 'Error'
 	"$rcon" shutdown 1 closing... | grep --ignore-case --invert-match 'Error'
 
 	printf 'Waiting for server to close... '
